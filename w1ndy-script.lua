f2a514b5d2d891da61a20fac3a94d6f07e575b65
22MAXPRO22 <ershovmax18@gmail.com>
2025-10-21 16:42:17 +0400

    new commit

--- w1ndy-script.lua
+++ w1ndy-script.lua
@@ -1,6 +1,4 @@
-﻿
-
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+﻿local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
 local Window = Library.CreateLib("Script By w1ndy", "RJTheme7")
 
